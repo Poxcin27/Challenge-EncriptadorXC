c69b7ca2c88085759d5178e136890c9f7a4f65cf
Poxcin27 <129027740+Poxcin27@users.noreply.github.com>
2024-03-18 23:48:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,127 @@
-var botonEncriptar = document.querySelector(".btn-encriptar");
-var botonDesencriptar = document.querySelector(".btn-desencriptar");
-var munieco = document.querySelector(".contenedormunieco");
-var contenedor = document.querySelector(".contenedor-parrafo");
+// Codigo para el uso del Challenge de Alura                                                    Poxcin Morales Jose Pedro
+
+// Declaracion de variables
+var encryptbutton = document.querySelector(".button-encrypt"); 
+var decryptbutton = document.querySelector(".button-decrypt");
+var personaconlupa = document.querySelector(".contenedor-derecho");
+var contenedor = document.querySelector(".contenedor-infderecho");
 var resultado = document.querySelector(".texto-resultado");
 
-botonEncriptar.onclick = encriptar;
-botonDesencriptar.onclick = desencriptar;
+// Asigna funciones a los eventos de click de los botones
+encryptbutton.onclick = encrypt;
+decryptbutton.onclick = decrypt;
 
-function encriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = encriptarTexto(cajatexto);
+// Funcion para encriptar el texto
+function encrypt() {
+    ocultarAdelante(); // Oculta ciertos elementos en la interfaz
+    var cajatexto = recuperarTexto(); // Obtiene el texto de entrada del usuario
+    resultado.textContent = encryptText(cajatexto); // Aplica la encriptacion al texto y lo muestra en la interfaz
 }
 
-function desencriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = desencriptarTexto(cajatexto);
+// Funcion para desencriptar el texto
+function decrypt() {
+    ocultarAdelante(); // Oculta ciertos elementos en la interfaz
+    var cajatexto = recuperarTexto(); // Obtiene el texto de entrada del usuario
+    resultado.textContent = decryptText(cajatexto); // Aplica la desencriptacion al texto y lo muestra en la interfaz
 }
 
-function recuperarTexto(){
-    var cajatexto = document.querySelector(".cajatexto")
-    return cajatexto.value
+// Funcion para recuperar el texto ingresado por el usuario
+function recuperarTexto() {
+    var cajatexto = document.querySelector(".cajadetexto");
+    return cajatexto.value;
 }
 
-function ocultarAdelante(){
-    munieco.classList.add("ocultar");
+// Funcion para ocultar ciertos elementos en la interfaz
+function ocultarAdelante() {
+    personaconlupa.classList.add("ocultar");
     contenedor.classList.add("ocultar");
 }
 
-function encriptarTexto(mensaje){
+// Funcion para encriptar el texto segun un conjunto de reglas
+function encryptText(mensaje) {
     var texto = mensaje;
     var textoFinal = "";
 
-    for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "ai"
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "enter"
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "imes"
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "ober"
-        }
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "ufat"
-        }
-        else{
-            textoFinal = textoFinal + texto[i]
+    for (var i = 0; i < texto.length; i++) {
+        // Aplica la encriptacion segun las reglas establecidas
+        if (texto[i] === "a") {
+            textoFinal += "ai";
+        } else if (texto[i] === "e") {
+            textoFinal += "enter";
+        } else if (texto[i] === "i") {
+            textoFinal += "imes";
+        } else if (texto[i] === "o") {
+            textoFinal += "ober";
+        } else if (texto[i] === "u") {
+            textoFinal += "ufat";
+        } else {
+            textoFinal += texto[i];
         }
     }
     return textoFinal;
-
 }
 
-function desencriptarTexto(mensaje){
+// Funcion para desencriptar el texto segun un conjunto de reglas
+function decryptText(mensaje) {
     var texto = mensaje;
     var textoFinal = "";
 
-    for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "a"
-            i = i+1;
+    for (var i = 0; i < texto.length; i++) {
+        // Aplica la desencriptacion segun las reglas establecidas
+        switch (texto[i]) {
+            case "a":
+                if (texto[i+1] === "i") {
+                    textoFinal += "a";
+                    i += 1; // Salta al siguiente caracter despues de 'i'
+                } else {
+                    textoFinal += texto[i];
+                }
+                break;
+            case "i":
+                if (texto[i+1] === "m" && texto[i+2] === "e" && texto[i+3] === "s") {
+                    textoFinal += "i";
+                    i += 3;
+                } else {
+                    textoFinal += texto[i];
+                }
+                break;
+            case "e":
+                if (texto[i+1] === "n" && texto[i+2] === "t" && texto[i+3] === "e" && texto[i+4] === "r") {
+                    textoFinal += "e";
+                    i += 4;
+                } else {
+                    textoFinal += texto[i];
+                }
+                break;
+            case "o":
+                if (texto[i+1] === "b" && texto[i+2] === "e" && texto[i+3] === "r") {
+                    textoFinal += "o";
+                    i += 3;
+                } else {
+                    textoFinal += texto[i];
+                }
+                break;
+            case "u":
+                if (texto[i+1] === "f" && texto[i+2] === "a" && texto[i+3] === "t") {
+                    textoFinal += "u";
+                    i += 3;
+                } else {
+                    textoFinal += texto[i];
+                }
+                break;
+            default:
+                textoFinal += texto[i];
         }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "e"
-            i = i+4;
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "i"
-            i = i+3;
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "o"
-            i = i+3;
-        }
-        
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "u"
-            i = i+3;
-        }
-        else{
-            textoFinal = textoFinal + texto[i];
-        }
-        
     }
-
     return textoFinal;
-
 }
 
-const btnCopiar = document.querySelector(".btn-copiar"); 
-    btnCopiar.addEventListener("click", copiar = () => {
+// Funcion para copiar el texto resultante al portapapeles
+const btnCopiar = document.querySelector(".copybuttton");
+btnCopiar.addEventListener("click", copiar);
+
+function copiar() {
     var contenido = document.querySelector(".texto-resultado").textContent;
     navigator.clipboard.writeText(contenido);
-    console.log("hola"); 
-});
+    console.log("hola");
+}
